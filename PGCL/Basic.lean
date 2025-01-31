8ee14fb42b6f4d2817c205e35460b3d1f7d4c6b7
oeb25 <oliverboving@gmail.com>
2025-01-31 15:24:14 +0100

    Fix remaining renames of OMDP

--- PGCL/Basic.lean
+++ PGCL/Basic.lean
@@ -2,6 +2,6 @@ import MDP.Cost
 import PGCL.OMDP
 
 theorem pGCL.iSup_iInf_EC_eq_wp [DecidableEq ϖ] :
-  ⨅ 𝒮, ⨆ n, (OMDP (ϖ:=ϖ)).EC (OMDP.cost X) 𝒮 (·⟨C,σ⟩) n = C.wp X σ
+  ⨅ 𝒮, ⨆ n, (𝒬 (ϖ:=ϖ)).EC (𝒬.cost X) 𝒮 (·⟨C,σ⟩) n = C.wp X σ
 := by
-  simp [← MDP.iSup_iInf_EC_eq_iInf_iSup_EC, MDP.iSup_iInf_EC_eq_lfp_Φ, ← dop_eq_wp, dop]
+  simp [← MDP.iSup_iInf_EC_eq_iInf_iSup_EC, MDP.iSup_iInf_EC_eq_lfp_Φ, ← op_eq_wp, op]
