936ce41e2a895d6f7e1a9d4293f55540366fa6a6
oeb25 <oliverboving@gmail.com>
2026-04-30 09:55:55 +0200

    Refactor `NatLog` example to use `tick`

--- HeyVL.lean
+++ HeyVL.lean
@@ -4,20 +4,19 @@ open Optimization.Notation
 open HeyLo
 
 abbrev NatLog.y : Ident := ⟨"y", .Nat⟩
-abbrev NatLog.c : Ident := ⟨"c", .Nat⟩
 
 open NatLog
 
 def NatLog := vc[𝒟, wp]
-  { ↑c + [0 < y] * ↑(y + nlog2(y)) }
-    while 0 < y inv(↑c + [0 < y] * ↑(y + nlog2(y))) {
-      { y := y / 2 } [1/2] { y := y - 1 } ; c := c + 1
+  { [0 < y] * ↑(y + nlog2(y)) }
+    while 0 < y inv([0 < y] * ↑(y + nlog2(y))) {
+      { y := y / 2 } [1/2] { y := y - 1 } ; tick(1)
     }
-  { ↑c }
+  { 0 }
 
 theorem NatLog.soundess : NatLog.sound := by
   vc_simp
-  rintro c (_ | y) <;> simp
+  rintro (_ | y) <;> simp
   grw [(by omega : 1 ≤ y ↔ 0 < y), (by omega : (y + 1) / 2 ≤ y + 1)]
   grw [(by gcongr; omega : y.log2 ≤ (y + 1).log2)]
   simp [← ENNReal.toReal_le_toReal, ENNReal.mul_eq_top, ENNReal.toReal_add]
